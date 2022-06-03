76b5c5dda0c487a72b45a14a2837dba790f1b688
RaphaelRMOB <63121662+RaphaelRMOB@users.noreply.github.com>
2022-06-03 15:35:33 -0300

    Done Tittle
    For now, the tittle screen is done.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuManager : MonoBehaviour
+{
+    
+    public Button startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    
+
+    public void Exit()
+    {
+        // Condição para fechar o jogo compilado e no Unity.
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+
+    }
+
+    
+}
