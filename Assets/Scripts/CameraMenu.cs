1e644f573da954905cc992eec3574834cae92302
RaphaelRMOB <63121662+RaphaelRMOB@users.noreply.github.com>
2022-07-08 12:49:27 -0300

    Menu buiulding
    Creating interaction with the camera on the game menu.

--- Assets/Scripts/CameraMenu.cs
+++ Assets/Scripts/CameraMenu.cs
@@ -6,38 +6,76 @@ public class CameraMenu : MonoBehaviour
 {
     //Criado um empty object para servir de referencia do local onde a camera irá parar.
     public GameObject camPosOptions;
-    public GameObject cube;
+    public GameObject camPosMain;
+    public GameObject aimVision;
     public GameObject menu;
+    public GameObject menuOption;
     bool options;
+    bool mainMenu;
     Vector3 camOptPos;
+    Vector3 camMainPos;
     public float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
+        menuOption.SetActive(false);
         camOptPos = camPosOptions.transform.position;
+        camMainPos = camPosMain.transform.position;
+        mainMenu = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (options == true) 
+        if (options == true)
         {
             transform.position = Vector3.MoveTowards(transform.position, camOptPos, speed * Time.deltaTime);
 
-            var newRotation = Quaternion.LookRotation(cube.transform.position - transform.position);
+            var newRotation = Quaternion.LookRotation(aimVision.transform.position - transform.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, speed * Time.deltaTime);
         }
-        
+
+        if (mainMenu == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, camMainPos, speed * Time.deltaTime);
+
+            var newRotation = Quaternion.LookRotation(aimVision.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, speed * Time.deltaTime);
+        }
+
     }
 
     public void MenuOptions()
     {
-        //https://answers.unity.com/questions/971927/how-do-i-smoothly-move-my-camera-to-a-set-position.html
+
         //Ideia estraida desta pagina. Vamos usar um bollean para ativar a direção da camera.
         options = true;
+        mainMenu = false;
         menu.SetActive(false);
+        StartCoroutine(OpenMenuOption());
+        
 
     }
 
+    public void MainMenu()
+    {
+        mainMenu = true;
+        options = false;
+        menuOption.SetActive(false);
+        StartCoroutine(OpenMenuMain());
+        
+    }
+    IEnumerator OpenMenuOption()
+    {
+        yield return new WaitForSeconds(2);
+        menuOption.SetActive(true);
+
+    }
+
+    IEnumerator OpenMenuMain()
+    {
+        yield return new WaitForSeconds(2);
+        menu.SetActive(true);
+    }
 
 }
